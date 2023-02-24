c9404a9e63368b340c72bfe59b2f759fa32472f7
Jason Trenouth <jason.trenouth@gmail.com>
2023-02-24 13:22:01 +0000

    Add Tokeniser tests

--- LSharp/Driver.cs
+++ LSharp/Driver.cs
@@ -25,7 +25,7 @@ namespace LSharp
     {
         public static void Main(string[] args)
         {
-            foreach (var list in new Parser().Parse(new Tokeniser().Tokenise("(defun foo (x) (+ x 1)) (foo 10)")))
+            foreach (var list in new Parser().Parse(new Tokeniser().Tokenise("(defun foo (x) (+ x 1)) (foo 10.05) (concat \"a\" \"b\")")))
             {
                 new Printer().Print(list);
             }
--- LSharp/Tokeniser.cs
+++ LSharp/Tokeniser.cs
@@ -15,6 +15,17 @@ namespace LSharp
     { 
         internal TokenType Type;
         internal object? Value;
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Token t && Type.Equals(t.Type) && object.Equals(Value, t.Value);
+        }
+
+        public override string ToString()
+        {
+            var value = Value?.ToString() ?? "";
+            return $"<{Type} {value}>";
+        }
     }
 
     class Tokeniser
